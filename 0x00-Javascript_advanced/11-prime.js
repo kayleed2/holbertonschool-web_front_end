9ac48eabe4248523d286644d26223dd3fc172b4c
Kaylee Dao <kayleed2@gmail.com>
2022-07-15 09:42:07 -0500

    Using set timeout to call execution time

---
+++ 0x00-Javascript_advanced/11-prime.js
@@ -0,0 +1,21 @@
+function countPrimeNumbers() {
+    let primes = [];
+    for (let i = 2; i <= 100; i++) {
+        let flag = 1;
+        for (let n = 2; n < i; n++) {
+            if (i % n === 0) {
+                flag = 0;
+            }
+        }
+        if (flag === 1) {
+            primes.push(i);
+        }
+    }
+    return primes;
+}
+const t0 = performance.now();
+for (let i = 0; i <= 100; i++) {
+    countPrimeNumbers();
+}
+const t1 = performance.now();
+setTimeout(() => { console.log(`Execution time of calculating countPrimeNumbers 100 times was ${t1 - t0} milliseconds.`)}, 0);
